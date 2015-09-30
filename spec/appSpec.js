6612bd79acb90ac9939d2ac7e00ad7c8c4e87251
VoxWave <victor_bankowski@hotmail.com>
2015-09-30 22:01:31 +0300

    [ci skio]

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,38 +1,33 @@
-describe('EventController', function() {
-    beforeEach(module('af'));
+require('angular-mocks')
+describe('EventController test', function() {
+    beforeEach(angular.mock.module('af'));
 
-    var $controller;
+    var ctrl, scope, httpMock;
 
-    beforeEach(inject(function(_$controller_) {
-        $controller = _$controller_;
+    beforeEach(inject(function($controller, $rootScope, $httpBackend) {
+        httpMock = $httpBackend;
+
+        scope = $rootScope.$new();
+        httpMock.when('GET', 'http://kokous-backend.herokuapp.com/api/dev/events').respond('[{"name":"testi1","time":"123","place":"mettä"},{"name":"testi2","time":"132","place":"kallio"}]');
+        ctrl = $controller;
+        ctrl(EventController, {
+            $scope: scope
+        });
+
     }));
 
-    describe('$scope.getDetails', function() {
         it('sets ev.event to the the object at that index', function() {
-            var $scope = {};
-            var $controller = $controller('EventController', { $scope: $scope});
-            $scope.events = [
-                {
-                    "name": "testi1",
-                    "time": "123",
-                    "place": "mettä"
-                },
-                {
-                    "name": "testi2",
-                    "time": "132",
-                    "place": "kallio"
-                }
-            ];
+            httpMock.expectGET('http://kokous-backend.herokuapp.com/api/dev/events');
+            httpMock.flush();
 
-            $scope.getDetails(0);
+            scope.getDetails(0);
             expect($scope.event.name).toEqual('testi1');
             expect($scope.event.time).toEqual('123');
             expect($scope.event.place).toEqual('mettä');
 
-            $scope.getDetails(1);
+            scope.getDetails(1);
             expect($scope.event.name).toEqual('testi2');
             expect($scope.event.time).toEqual('132');
             expect($scope.event.place).toEqual('kallio');
             });
-        });
 });
