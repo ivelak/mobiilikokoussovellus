d80316a86b9ac7483481aecb6c8c3dcbb7acd630
Antti Rantapelkonen <rantaana@gmail.com>
2015-11-02 17:32:56 +0200

    lisätty toiminnallisuus aktiviteetin yksitysikohtien katsomiseen-ei toimi vielä

--- www/js/eventcontroller.js
+++ www/js/eventcontroller.js
@@ -4,11 +4,39 @@ app.controller('EventController', function EventController($scope, $http) {
     var ev = this;
     ev.firstName= "John";
     ev.lastName= "Doe";
-    $http.get("http://kokous-backend.herokuapp.com/api/dev/events").success(function(res){
+    $http.get("http://localhost:8000/api/dev/events").success(function(res){
                  ev.events = res;
     });
 
     ev.getDetails = function(index) {
         ev.event = ev.events[index];
+        
     }
+    ev.getDateTime = function(occ) {
+        var time=new Date(occ.time.date);
+        var date=new Date(occ.date.date);
+        
+        var timedate="";
+        timedate= date.toLocaleDateString() + " " + time.toLocaleTimeString();        //.substr(time.length-9, time.length-5);
+        
+        return timedate;
+    }
+    
+    ev.concat=function(string1, string2){
+        return string1+" "+string2;
+    }
+    ev.getActivity=function(index) {
+        var activityInfo = ev.event.activities[index];
+        $http.get("http://localhost:8000/api/dev/activities/"+activityInfo.id).success(function(res){
+           ev.activity=res; 
+        });
+    }
+    ev.getValueNames=function(object){
+        var values="";
+        for (i in object) {
+            values+=i[0].name;
+        }
+        return values;
+    }
+ 
 });
