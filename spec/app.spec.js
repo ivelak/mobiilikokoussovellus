b071bdc8288fe318d7b4b7f30b83bd3c3f5828c9
Leo Pynnonen <leo.pynnonen@helsinki.fi>
2015-11-05 15:52:51 +0200

    Korjaus

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -6,13 +6,13 @@ describe('EventController test', function() {
     beforeEach(inject(function($controller, $rootScope, $httpBackend) {
         httpMock = $httpBackend;
         scope = $rootScope.$new();
-        httpMock.when('GET', 'http://kokous-backend.herokuapp.com/api/dev/events').respond('[{"name":"testi1","time":"123","place":"mettä"},{"name":"testi2","time":"132","place":"kallio"}]');
+        httpMock.when('GET', 'http://localhost:8000/api/dev/events').respond('[{"name":"testi1","time":"123","place":"mettä"},{"name":"testi2","time":"132","place":"kallio"}]');
         ctrl = $controller('EventController', {$scope: scope});
 
     }));
 
         it('sets ev.event to the the object at that index', function() {
-            httpMock.expectGET('http://kokous-backend.herokuapp.com/api/dev/events');
+            httpMock.expectGET('http://localhost:8000/api/dev/events');
             httpMock.flush();
 
             ctrl.getDetails(0);
