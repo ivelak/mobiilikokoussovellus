b75a5c88883359f4100fc88b4f7a432dbb89f989
Leo Pynnonen <leo.pynnonen@helsinki.fi>
2015-11-05 16:48:14 +0200

    Segoilua

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,4 +1,4 @@
-describe('EventController test', function() {
+describe('Tests for eventOccurrences', function() {
     beforeEach(module('af'));
 
     var ctrl, scope, httpMock;
@@ -6,7 +6,7 @@ describe('EventController test', function() {
     beforeEach(inject(function($controller, $rootScope, $httpBackend) {
         httpMock = $httpBackend;
         scope = $rootScope.$new();
-        httpMock.when('GET', 'http://localhost:8000/api/dev/events').respond('[{"name":"testi1","time":"123","place":"mettä"},{"name":"testi2","time":"132","place":"kallio"}]');
+        httpMock.when('GET', 'http://localhost:8000/api/dev/events').respond('[{"id":"1","created_at":"2015-11-05 15:56:43","updated_at":"2015-11-05 15:56:43","cancelled":"0","date":{"date":"2015-12-12 15:56:43.000000","timezone_type":3,"timezone":"Europe\/Helsinki"},"time":{"date":"-0001-11-30 12:12:00.000000","timezone_type":3,"timezone":"Europe\/Helsinki"},"place":"Onkalo","notes":null,"event_id":"1","activities":[],"event":{"name":"Hippa","id":"1","time":"2015-12-12 12:12:00","endDate":"2015-12-12 15:56:43","place":"Onkalo","description":"","created_at":"2015-11-05 15:56:43","updated_at":"2015-11-05 15:56:43","group_id":"1"}}]');
         ctrl = $controller('EventController', {$scope: scope});
 
     }));
@@ -16,13 +16,32 @@ describe('EventController test', function() {
             httpMock.flush();
 
             ctrl.getDetails(0);
-            expect(ctrl.event.name).toEqual('testi1');
-            expect(ctrl.event.time).toEqual('123');
-            expect(ctrl.event.place).toEqual('mettä');
-
-            ctrl.getDetails(1);
-            expect(ctrl.event.name).toEqual('testi2');
-            expect(ctrl.event.time).toEqual('132');
-            expect(ctrl.event.place).toEqual('kallio');
+            expect(ctrl.event.event.name).toEqual('Hippa');
+            expect(ctrl.event.event.time).toEqual('2015-12-12 12:12:00');
+            expect(ctrl.event.event.place).toEqual('Onkalo');
             });
 });
+
+describe('Tests for activities', function() {
+        beforeEach(module('af'));
+
+    var ctrl, scope, httpMock;
+
+    beforeEach(inject(function($controller, $rootScope, $httpBackend) {
+        httpMock = $httpBackend;
+        scope = $rootScope.$new();
+        httpMock.when('GET', 'http://localhost:8000/api/dev/activities/0').respond('[{"id":"1","guid":"34c20bb4c990ed73bba8b7e1f15df76e","name":"Ruoka haltuun","age_group":"samoajat","created_at":"2015-11-05 15:55:49","updated_at":"2015-11-05 15:55:49"}]');
+        ctrl = $controller('EventController', {$scope: scope});
+        
+    }));
+    it('sets ev.activities to the the object at that index', function() {
+        ev.getActivity(0);
+        httpMock.expectGET('http://localhost:8000/api/dev/activities/0');
+            httpMock.flush();
+
+            ctrl.getDetails(0);
+            expect(ctrl.activity.name).toEqual('Ruoka haltuun');
+            expect(ctrl.activity.age_group).toEqual('samoajat');
+            expect(ctrl.activity.created_at).toEqual('2015-11-05 15:55:49');
+            });
+    });
