b708037baed2abe43d92187cb35a1a79d30d2a70
Antti Rantapelkonen <rantaana@gmail.com>
2015-11-23 17:58:43 +0200

    Lisätty suoritteiden merkitsemiselle linkki jonka takana listaus käyttäjistä ja occurrencen aktiviteetit alasvetovalikossa.

--- www/js/eventcontroller.js
+++ www/js/eventcontroller.js
@@ -2,13 +2,27 @@ var app = angular.module('af', []);
 
 app.controller('EventController', function EventController($scope, $http) {
     var ev = this;
-    $http.get("http://kokousbackendenv-env.elasticbeanstalk.com/api/dev/events").success(function(res){
+    var url="http://localhost:8000/api/dev/";
+    
+    $http.get("http://localhost:8000/api/dev/events").success(function(res){
                  ev.events = res;
+    //    $http.get("http://kokousbackendenv-env.elasticbeanstalk.com/api/dev/events").success(function(res){
+    //             ev.events = res;
     });
 
     ev.getDetails = function(index) {
         ev.event = ev.events[index];
         
+    }
+    
+    ev.getUsers=function(){
+        
+        var grid = ev.event.event.group.id;
+        console.log(ev.events);
+        $http.get("http://localhost:8000/api/dev/group/"+grid).success(function(res){
+            ev.users=res.users;
+        });
+       
     }
     ev.getDateTime = function(occ) {
         var time=new Date(occ.time.date);
@@ -25,10 +39,15 @@ app.controller('EventController', function EventController($scope, $http) {
     }
     ev.getActivity=function(index) {
         var activityInfo = ev.event.activities[index];
-        $http.get("http://kokousbackendenv-env.elasticbeanstalk.com/api/dev/activities/"+activityInfo.id).success(function(res){
+        $http.get("http://localhost:8000/api/dev/activities/"+activityInfo.id).success(function(res){
            ev.activity=res; 
+        //    $http.get("http://kokousbackendenv-env.elasticbeanstalk.com/api/dev/activities/"+activityInfo.id).success(function(res){
+        //   ev.activity=res; 
+            
+            
         });
     }
+    
     
  
 });
