5a8be0e92515b798b09e1ea435abd6ee973c1c45
Leo Pynnonen <leo.pynnonen@helsinki.fi>
2015-11-23 15:28:41 +0200

    Kirjautumis edistystä

--- www/js/eventcontroller.js
+++ www/js/eventcontroller.js
@@ -5,10 +5,14 @@ app.controller('EventController', function EventController($scope, $http) {
     $http.get("http://kokousbackendenv-env.elasticbeanstalk.com/api/dev/events").success(function(res){
                  ev.events = res;
     });
-
     ev.getDetails = function(index) {
         ev.event = ev.events[index];
         
+    }
+    ev.getUserActivities = function() {
+$http.get("http://kokousbackendenv-env.elasticbeanstalk.com/api/dev/userActivities").success(function(res)      {
+                 ev.userActivities = res;
+    });
     }
     ev.getDateTime = function(occ) {
         var time=new Date(occ.time.date);
